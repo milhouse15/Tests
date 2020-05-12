0e9cdc9b6d72c772880148b0c4673945e2bdda97
milhouse15 <flash22@outlook.com>
2020-05-12 06:40:04 -0700

    Update process.h

--- include/process.h
+++ include/process.h
@@ -13,7 +13,7 @@ class Process {
   int Pid();                               // TODO: See src/process.cpp
   std::string User();                      // TODO: See src/process.cpp
   std::string Command();                   // TODO: See src/process.cpp
-  float CpuUtilization() const;                  // TODO: See src/process.cpp
+  float CpuUtilization();                  // TODO: See src/process.cpp
   std::string Ram();                       // TODO: See src/process.cpp
   long int UpTime();                       // TODO: See src/process.cpp
   bool operator<(Process const& a) const;  // TODO: See src/process.cpp
@@ -24,7 +24,7 @@ class Process {
     pid_ = pid;
     user_ = LinuxParser::User(pid);
     command_ = LinuxParser::Command(pid);
-    cpu_ = LinuxParser::CpuUtilization2(pid);
+    cpu_ = LinuxParser::CpuUtilization(pid);
     ram_ = LinuxParser::Ram(pid);
     uptime_ = LinuxParser::UpTime(pid);
   }
